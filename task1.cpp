69e70afb64be4adb97265aa3c79623a56894d82b
ShreyaSrivastava12 <91174445+ShreyaSrivastava12@users.noreply.github.com>
2021-10-06 09:59:58 +0530

    Add files via upload

---
+++ task1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+      string a="cplusplus";
+      string b="programming";
+      int c=4;
+      int d=5;
+      //int res=c+d;
+      //string res1=a+b;
+      string res2=a+c;
+      //cout<<res<<endl;
+      //cout<<res1<<endl;
+      cout<<res2<<endl;
+}
